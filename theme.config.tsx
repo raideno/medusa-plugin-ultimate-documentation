9669d8eeac841a5f2fb72014229e9776b7c87fa8
raiden <nadirkichou@hotmail.fr>
2023-10-31 23:15:53 +0100

    :tada: experimenting

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Medusa Plugin Ultimate</span>,
+  head: (
+    <div className="w-full py-2 flex flex-row items-center justify-center bg-[#ab570a]">
+      <div className="font-bold text-white">
+        ⚠️IMPORTANT⚠️: The doc is still being built / completed, feel free to DM
+        if you need help.
+      </div>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    icon: undefined,
+    link: "https://github.com/raideno/medusa-plugin-ultimate",
   },
   chat: {
-    link: 'https://discord.com',
+    icon: undefined,
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/raideno/medusa-plugin-ultimate",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Medusa Ultimate Plugin Docs",
   },
-}
+};
 
-export default config
+export default config;
