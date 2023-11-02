3eb6bae8451320641bdbe17f06367bbd850c72e8
raiden <nadirkichou@hotmail.fr>
2023-11-02 15:20:18 +0100

    :construction: working on documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,14 +3,14 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>Medusa Plugin Ultimate</span>,
-  head: (
-    <div className="w-full py-2 flex flex-row items-center justify-center bg-[#ab570a]">
-      <div className="font-bold text-white">
-        ⚠️IMPORTANT⚠️: The doc is still being built / completed, feel free to DM
-        if you need help.
-      </div>
-    </div>
-  ),
+  // head: (
+  //   <div className="w-full py-2 px-6 flex flex-row items-center justify-center bg-[#ab570a]">
+  //     <div className="font-[14px] sm:font-[16px] text-white text-center">
+  //       ⚠️IMPORTANT⚠️: The doc is still being built / completed, feel free to DM
+  //       if you need help.
+  //     </div>
+  //   </div>
+  // ),
   project: {
     icon: undefined,
     link: "https://github.com/raideno/medusa-plugin-ultimate",
